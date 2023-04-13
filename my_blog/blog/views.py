604b922c159714b3b1e35e90174e2c57592a9113
Siarhei <siarheiburdzeika@gmail.com>
2023-04-13 17:10:23 +0200

    added home page

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import Post
 
-# Create your views here.
+
+class PostView(View):
+
+    def get(self, request):
+        posts = Post.objects.all()
+        return render(request, 'home.html', {'post_list': posts})
+
